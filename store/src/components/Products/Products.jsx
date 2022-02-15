d1e0ada4ec2bb3a98829dde1ecafa95307d89290
Mohammad Afandi <afandimohammad@hotmail.com>
2022-02-15 01:07:31 -0500

    Fixed few bugs

--- store/src/components/Products/Products.jsx
+++ store/src/components/Products/Products.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import Grid from '@material-ui/core'
+import {Grid} from '@material-ui/core'
+
+import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id:1, name: "Shoes", description: 'Running shoes.', price:'$100' },
@@ -8,10 +11,14 @@ const products = [
 ]
 
 const Products = () => {
-    <main>
-        <Grid container justify="center" spacing ={4}>
+    const classes = useStyles();
+
+    return(
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Grid container justifyContent="center" spacing ={4}>
             {products.map((product) => (
-                <Grid item key = {products.id} xs={12} sm={6} md={4} lg={3}>
+                <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
                     <Product product = {product}/>
                 </Grid>
             ))}
