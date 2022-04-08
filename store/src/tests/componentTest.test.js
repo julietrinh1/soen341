5d94bb101f6ec5af9837060dfa537329608aa33a
Mohammad Afandi <afandimohammad@hotmail.com>
2022-04-07 23:54:07 -0400

    Tests were failing, fixed it

--- store/src/tests/componentTest.test.js
+++ store/src/tests/componentTest.test.js
@@ -10,6 +10,7 @@ import SignUp from '../components/Login-Register Pages/registerPage';
 import Products from '../components/Products/Products';
 import Product from '../components/Products/Product/Product';
 import Orders from '../components/Orders';
+import axios from 'axios';
 
 jest.mock('axios', () => {
     return {
@@ -22,47 +23,56 @@ jest.mock('axios', () => {
 jest.mock('axios', () => ({ get: jest.fn(),post: jest.fn(), create: jest.fn() }));
 
 it('renders home page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByText } = render(<Home />);
     expect(getByText(/Welcome/i)).toBeInTheDocument();
 })
 
 it('render checkout page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByTestId } = render(<Checkout />);
     expect(getByTestId("formForCheckout")).toBeInTheDocument();
 })
 
 it('render cart page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByText } = render(<Cart />);
     expect(getByText(/You have no items in your shopping cart!/i)).toBeInTheDocument();
 })
 
 it('render dashboard page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByText } = render(<Dashboard />);
     expect(getByText(/This is your dashboard./i)).toBeInTheDocument();
 })
 
 it('render sign in page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getAllByText } = render(<SignIn />);
     expect(getAllByText(/Sign In/i)[0]).toBeInTheDocument();
 })
 
 
 it('render sign up page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByText } = render(<SignUp />);
     expect(getByText(/Already have an account\? Sign in/i)).toBeInTheDocument();
 })
 
 it('render products page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByTestId } = render(<Products />);
     expect(getByTestId("productPageTestId")).toBeInTheDocument();
 })
 
 it('render product card contents', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByTestId } = render(<Product />);
     expect(getByTestId("productCardContentsTest")).toBeInTheDocument();
 })
 
 it('render orders page', () => {
+    axios.get.mockResolvedValue('{"test": "test"}');
     const { getByTestId } = render(<Orders />);
     expect(getByTestId("orderPageTestId")).toBeInTheDocument();
 })
